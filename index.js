9e6efb2a3470e5d9705c7313453fbe28a9a7a8ed
Jonathan Eiten <jonathan.eiten@mojotouch.com>
2017-12-26 22:27:10 -0500

    was always duck-typing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = isNode
 function isNode (val) {
   return (!val || typeof val !== 'object')
     ? false
-    : (typeof window === 'object' && typeof window.Node === 'object')
+    : (typeof window === 'object' && typeof window.Node === 'function')
       ? (val instanceof window.Node)
       : (typeof val.nodeType === 'number') &&
         (typeof val.nodeName === 'string')

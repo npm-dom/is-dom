01b41a03823ea11c1a1882d04576f093e443a6ee
Steven D Vachon <contact@svachon.com>
2019-06-13 11:23:25 -0400

    Bump versions

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-var jsdom = require('jsdom').jsdom
+var JSDOM = require('jsdom').JSDOM
 var tape = require('tape')
 
 var isDom = require('./index')
 
 tape('is-dom', function (t) {
-  global.document = jsdom('<html><body></body></html>')
-  global.window = document.parentWindow
+  var dom = new JSDOM('<html><body></body></html>')
+  global.window = dom.window
+  global.document = window.document
 
   t.test('should check if supplied argument is a dom node', function (t) {
     t.plan(13)
@@ -19,7 +20,7 @@ tape('is-dom', function (t) {
     t.equal(isDom('foo'), false)
     t.equal(isDom(/asda/), false)
     t.equal(isDom({}), false)
-    t.equal(isDom({nodeType: 1, nodeName: 'BODY'}), true)
+    t.equal(isDom({ nodeType: 1, nodeName: 'BODY' }), true)
     t.equal(isDom(global.window), false)
     t.equal(isDom(global.document), true)
   })

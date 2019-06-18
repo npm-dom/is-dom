077ba87fe436bd5d36b5be29a7b110bb5bf6a92f
Josh Duff <me@JoshDuff.com>
2019-06-18 09:03:42 -0500

    Simplify logic by removing nested ternaries
    Also dropped the use strict line

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-'use strict'
 var isObject = require('is-object')
 var isWindow = require('is-window')
 
 function isNode (val) {
-  return !isObject(val)
-    ? false
-    : (!isWindow(window) || typeof window.Node !== 'function')
-      ? false
-      : (typeof val.nodeType === 'number') &&
-        (typeof val.nodeName === 'string')
+  if (!isObject(val) || !isWindow(window) || typeof window.Node !== 'function') {
+    return false
+  }
+
+  return typeof val.nodeType === 'number' &&
+    typeof val.nodeName === 'string'
 }
 
 module.exports = isNode

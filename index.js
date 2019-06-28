9573937dd70bbc614bd53b75710fe5094b6cefe6
Josh Duff <me@JoshDuff.com>
2019-06-27 22:55:39 -0500

    Test in a real browser (err, I mean, Electron)
    Inspired by #11

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var isObject = require('is-object')
-var isWindow = require('is-window')
+const isObject = require(`is-object`)
+const isWindow = require(`is-window`)
 
-function isNode (val) {
-  if (!isObject(val) || !isWindow(window) || typeof window.Node !== 'function') {
-    return false
-  }
+function isNode(val) {
+	if (!isObject(val) || !isWindow(window) || typeof window.Node !== `function`) {
+		return false
+	}
 
-  return typeof val.nodeType === 'number' &&
-    typeof val.nodeName === 'string'
+	return typeof val.nodeType === `number`
+		&& typeof val.nodeName === `string`
 }
 
 module.exports = isNode
--- test.js
+++ test.js
@@ -1,35 +1,26 @@
 'use strict'
-var JSDOM = require('jsdom').JSDOM
-var tape = require('tape')
+const test = require(`tape`)
 
-var isDom = require('./index')
+const isDom = require(`./index.js`)
 
-tape('is-dom', function (t) {
-  var currentRealm = new JSDOM('<html><body></body></html>')
-  var otherRealm = new JSDOM('<html><body></body></html>')
-  global.window = currentRealm.window
-  global.document = currentRealm.window.document
+test(`is-dom`, t => {
+	t.plan(14)
+	console.log(`wat`)
+	t.equal(isDom(null), false)
+	t.equal(isDom(null), false)
+	t.equal(isDom(false), false)
+	t.equal(isDom(new Date()), false)
 
-  t.test('should check if supplied argument is a dom node', function (t) {
-    t.plan(17)
-    t.equal(isDom(null), false)
-    t.equal(isDom(null), false)
-    t.equal(isDom(false), false)
-    t.equal(isDom(new Date()), false)
-    t.equal(isDom(), false)
-    t.equal(isDom([]), false)
-    t.equal(isDom(2), false)
-    t.equal(isDom('foo'), false)
-    t.equal(isDom(/asda/), false)
-    t.equal(isDom({}), false)
+	t.equal(isDom(), false)
+	t.equal(isDom([]), false)
+	t.equal(isDom(2), false)
+	t.equal(isDom(`foo`), false)
+	t.equal(isDom(/asda/), false)
+	t.equal(isDom({}), false)
 
-    t.equal(isDom({ nodeType: 1, nodeName: 'BODY' }), true)
-    t.equal(isDom(document.createElement('body')), true)
-    t.equal(isDom(window), false)
-    t.equal(isDom(document), true)
 
-    t.equal(isDom(otherRealm.window), false)
-    t.equal(isDom(otherRealm.window.document), true)
-    t.equal(isDom(otherRealm.window.document.createElement('body')), true)
-  })
+	t.equal(isDom({ nodeType: 1, nodeName: `BODY` }), true)
+	t.equal(isDom(document.createElement(`body`)), true)
+	t.equal(isDom(window), false)
+	t.equal(isDom(document), true)
 })
